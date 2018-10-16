cc6b2c9504f3cab99c7832b8f092dcedffa72b0c
lackingSemicolon <43654181+lackingSemicolon@users.noreply.github.com>
2018-10-16 16:55:31 -0700

    update to init
    Installed all the prereq but still has error

---
+++ Scrape.js
@@ -0,0 +1,10 @@
+const request = require('request');
+const cheerio = require('cheerio');
+
+request('https://www.youtube.com/playlist?list=PL3s-49gjr4ytMR_tAm69AdFEniayIT_q8', function (error, response, html) {
+  if (!error && response.statusCode === 200) {
+    const $ = cheerio.load(html);
+    const parsedHTML = $.load('<html><body><script></script></body></html>');
+    console.log(parsedHTML);
+  }
+});
