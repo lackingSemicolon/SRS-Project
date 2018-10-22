a71c2fd84d30fc11bdf7be7fe6bc368dbf6676f3
lackingSemicolon <43654181+lackingSemicolon@users.noreply.github.com>
2018-10-21 20:18:47 -0700

    add readme

---
+++ README.txt
@@ -0,0 +1,11 @@
+Description:
+This project will be similar to redbox with a combination of imdb. It'll display a list of movies where users can click on a movie to see a description
+and a trailer of the movie.
+
+Team Member:
+Tam Nguyen
+Kyle Voos
+Andres Gonzales-Flores
+
+Status:
+Designing phase
--- Scrape.js
+++ Scrape.js
@@ -1,5 +1,17 @@
-const request = require('request');
-const cheerio = require('cheerio');
+var requirejs = require('requirejs');
+
+requirejs.config({
+  //load the mode modules to top level JS file
+  //by passing the top level main.js require function to requirejs
+  nodeRequire: require,
+});
+var request;
+var cheerio;
+requirejs(['request', 'cheerio'],
+  function (name1, name2) {
+    request = name1;
+    cheerio = name2;
+  });
 
 request('https://www.youtube.com/playlist?list=PL3s-49gjr4ytMR_tAm69AdFEniayIT_q8', function (error, response, html) {
   if (!error && response.statusCode === 200) {
