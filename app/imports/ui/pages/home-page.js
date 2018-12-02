117319d11b3802109a6d4ad7353475692bfa57c4
lackingSemicolon <43654181+lackingSemicolon@users.noreply.github.com>
2018-12-01 19:52:45 -0800

    fixed header
    add spacing and fixed picture overlapping the header

--- app/imports/ui/pages/home-page.js
+++ app/imports/ui/pages/home-page.js
@@ -12,6 +12,18 @@ Template.Home_Page.helpers({
   horrorList() {
     return Movie.find({ Genre: 'Horror' }, { limit: 6 });
   },
+  actionList() {
+    return Movie.find({ Genre: 'Action' }, { limit: 6 });
+  },
+  comedyList() {
+    return Movie.find({ Genre: 'Comedy' }, { limit: 6 });
+  },
+  romanceList() {
+    return Movie.find({ Genre: 'Romance' }, { limit: 6 });
+  },
+  trendingList() {
+    return Movie.find({}, { limit: 6 });
+  },
   /**
    * @returns {*} All of the Stuff documents.
    */
